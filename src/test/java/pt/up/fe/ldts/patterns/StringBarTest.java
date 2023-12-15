077fd1b911d8bcffdbef39339501951aa2f27789
Rodrigo Miranda <rucamir2004@gmail.com>
2023-12-15 19:37:26 +0000

    the rest, i forgot

---
+++ src/test/java/pt/up/fe/ldts/patterns/StringBarTest.java
@@ -0,0 +1,187 @@
+package pt.up.fe.ldts.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pt.up.fe.ldts.patterns.StringDrink;
+import pt.up.fe.ldts.patterns.StringRecipe;
+import pt.up.fe.ldts.patterns.bar.Bar;
+import pt.up.fe.ldts.patterns.bar.client.FerengiClient;
+import pt.up.fe.ldts.patterns.bar.client.HumanClient;
+import pt.up.fe.ldts.patterns.bar.StringBar;
+import pt.up.fe.ldts.patterns.bar.client.RomulanClient;
+import pt.up.fe.ldts.patterns.bar.strategy.ImpatientStrategy;
+import pt.up.fe.ldts.patterns.bar.strategy.SmartStrategy;
+import pt.up.fe.ldts.patterns.transformer.StringCaseChanger;
+import pt.up.fe.ldts.patterns.transformer.StringInverter;
+import pt.up.fe.ldts.patterns.transformer.StringReplacer;
+import pt.up.fe.ldts.patterns.transformer.StringTransformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringBarTest {
+    @Test
+    public void happyHour() {
+        Bar bar = new StringBar();
+        assertFalse(bar.isHappyHour());
+
+        bar.startHappyHour();
+        assertTrue(bar.isHappyHour());
+
+        bar.endHappyHour();
+        assertFalse(bar.isHappyHour());
+    }
+
+    @Test
+    public void addObserver() {
+        Bar bar = new StringBar();
+
+        HumanClient clientMock = Mockito.mock(HumanClient.class);
+        bar.addObserver(clientMock);
+
+        Mockito.verify(clientMock, Mockito.never()).happyHourStarted(bar);
+        Mockito.verify(clientMock, Mockito.never()).happyHourEnded(bar);
+
+        bar.startHappyHour();
+        Mockito.verify(clientMock, Mockito.times(1)).happyHourStarted(bar);
+        Mockito.verify(clientMock, Mockito.never()).happyHourEnded(bar);
+
+        bar.endHappyHour();
+        Mockito.verify(clientMock, Mockito.times(1)).happyHourStarted(bar);
+        Mockito.verify(clientMock, Mockito.times(1)).happyHourEnded(bar);
+    }
+
+    @Test
+    public void removeObserver() {
+        Bar bar = new StringBar();
+
+        HumanClient clientMock = Mockito.mock(HumanClient.class);
+        bar.addObserver(clientMock);
+        bar.removeObserver(clientMock);
+
+        bar.startHappyHour();
+        bar.endHappyHour();
+
+        Mockito.verify(clientMock, Mockito.never()).happyHourStarted(bar);
+        Mockito.verify(clientMock, Mockito.never()).happyHourEnded(bar);
+    }
+
+    private StringRecipe getRecipe() {
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        StringReplacer sr = new StringReplacer('A', 'X');
+
+        List<StringTransformer> transformers = new ArrayList<>();
+        transformers.add(si);
+        transformers.add(cc);
+        transformers.add(sr);
+
+        StringRecipe recipe = new StringRecipe(transformers);
+        return recipe;
+    }
+
+    @Test
+    public void orderStringRecipe() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        stringBar.order(drink, recipe);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void impatientStrategy() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        ImpatientStrategy strategy = new ImpatientStrategy();
+        HumanClient client = new HumanClient(strategy);
+
+        // Recipe is ordered immediately
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void smartStrategyStartOpened() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        SmartStrategy strategy = new SmartStrategy();
+        HumanClient client = new HumanClient(strategy);
+
+        // Recipe is ordered immediately as happy hour was already under way
+        stringBar.startHappyHour();
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void smartStrategyStartClosed() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        SmartStrategy strategy = new SmartStrategy();
+        HumanClient client = new HumanClient(strategy);
+        stringBar.addObserver(client); // this is important!
+
+        client.wants(drink, recipe, stringBar);
+        assertEquals("AbCd-aBcD", drink.getText());
+
+        // Recipe is only ordered here
+        stringBar.startHappyHour();
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void ferengiAlreadyOpened() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        FerengiClient client = new FerengiClient();
+
+        // Recipe is ordered immediately
+        stringBar.startHappyHour();
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void ferengiStartClosed() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        FerengiClient client = new FerengiClient();
+        stringBar.addObserver(client); // this is important!
+
+        client.wants(drink, recipe, stringBar);
+        assertEquals("AbCd-aBcD", drink.getText());
+
+        // Recipe is only ordered here
+        stringBar.startHappyHour();
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    @Test
+    public void romulan() {
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = getRecipe();
+
+        RomulanClient client = new RomulanClient();
+
+        // Recipe is ordered immediately
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+
+}
